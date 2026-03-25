dda6ab63d74e3d07aa03d87f6799d71c9161598b
yashkumargupta12345 <guptayashyg45227@gmail.com>
2026-03-25 22:04:38 +0530

    Add user feature

---
+++ buggy_code.py
@@ -0,0 +1,11 @@
+def get_user(user_id):
+    query = "SELECT * FROM users WHERE id = " + user_id
+    result = db.execute(query)
+    password = result['password']
+    print("User password is: " + password)
+    return result
+
+def divide(a, b):
+    return a / b  # b=0 hoga toh crash!
+
+API_KEY = "sk-1234567890abcdef"  # hardcoded secret
